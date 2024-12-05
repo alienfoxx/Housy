16738a44850ab03f76046e2a932f50e892299468
alienfoxx <alienfoxx001@gmail.com>
2024-12-05 17:46:19 +0100

    fixing the mongoDb server conection bug "the moongose version is the cause "

--- api/index.js
+++ api/index.js
@@ -1,7 +1,25 @@
 import express from 'express';
- 
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+mongoose.set('debug', true);
+
+// Connect to MongoDB using the connection string from .env
+mongoose.connect(process.env.MONGO)
+.then(() => {
+    console.log('Connected to MongoDB!');
+})
+.catch((err) => {
+    console.error('Could not connect to MongoDB:', err);
+});
+
+// Create an Express app
 const app = express();
 
-app.listen(3000,()=>{
-    console.log('server is running on port 3000')
-})
+// Start the server
+app.listen(8000, () => {
+    console.log('Server is running on port 3000');
+});
