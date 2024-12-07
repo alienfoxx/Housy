919dd3f29f8263f029f15a853b6ef060227efbf7
alienfoxx <alienfoxx001@gmail.com>
2024-12-07 18:08:47 +0100

    create an midllewear and function for handling possible erorrs

--- api/index.js
+++ api/index.js
@@ -1,32 +1,43 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 // Load environment variables from .env file
 dotenv.config();
 
-mongoose.set('debug', true);
+mongoose.set("debug", true);
 
 // Connect to MongoDB using the connection string from .env
-mongoose.connect(process.env.MONGO)
-.then(() => {
-    console.log('Connected to MongoDB!');
-})
-.catch((err) => {
-    console.error('Could not connect to MongoDB:', err);
-});
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.error("Could not connect to MongoDB:", err);
+  });
 
 // Create an Express app
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 // Start the server
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
 
-app.use("/api/user", userRouter)
-app.use("/api/auth",authRouter)
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
