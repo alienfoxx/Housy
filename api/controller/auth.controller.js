9823f2530ca6162f6763ed0242c9393617bc6abb
alienfoxx <alienfoxx001@gmail.com>
2024-12-12 20:01:35 +0100

    complete and fixing the signin auth isuess

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -3,7 +3,7 @@ import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 import jwt from "jsonwebtoken";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res,next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
@@ -13,25 +13,50 @@ export const signup = async (req, res) => {
     res.status(201).json("User created successfully!");
   } catch (error) {
     next(error);
-  }
+  }  
 };
 
 // function for the sign-in route
-
 export const signin = async (req, res, next) => {
-  const { email, password } = req.body;
-  try {
-    const validUser = await User.findOne({ email });
-    if (!validUser) return next(errorHandler(404, "User not found!"));
-    const validPassword = bcryptjs.compareSync(password, validUser.password);
-    if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-    const {password : pass ,...rest} = validUser._doc;
-    res
-      .cookie("access_token", token, { httpOnly: true })
-      .status(200)
-      .json(rest);
-  } catch (error) {
-    next(error);
-  }
-};
+    const { email, password } = req.body;
+    console.log("Request Body:", req.body);
+    try {
+      const validUser = await User.findOne({ email });
+      if (!validUser){
+        console.log("User not found for email:", email);
+         return next(errorHandler(404, 'User not found!'));
+      }
+      const validPassword = bcryptjs.compareSync(password, validUser.password);
+      if (!validPassword){
+        console.log("User not found for email:", email); 
+        return next(errorHandler(401, 'Wrong credentials!'));
+      }
+      const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+      const { password: pass, ...rest } = validUser._doc;
+      res
+        .cookie('access_token', token, { httpOnly: true })
+        .status(200)
+        .json(rest);
+    } catch (error) {
+      console.error("Signin Error:", error)
+      next(error);
+    }
+  };
+
+// export const signin = async (req, res, next) => {
+//   const { email, password } = req.body;
+//   try {
+//     const validUser = await User.findOne({ email });
+//     if (!validUser) return next(errorHandler(404, "User not found!"));
+//     const validPassword = bcryptjs.compareSync(password, validUser.password);
+//     if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
+//     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+//     const {password : pass ,...rest} = validUser._doc;
+//     res
+//       .cookie("access_token", token, { httpOnly: true })
+//       .status(200)
+//       .json(rest);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
